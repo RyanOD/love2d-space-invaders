8ab00d9b2c817b4ae52506bd51744184cef1edb1
RyanOD <odonnell.joseph@gmail.com>
2020-11-19 08:00:22 -0800

    Lasers added to screen with table removal.

--- src/classes/Laser.lua
+++ src/classes/Laser.lua
@@ -1,10 +1,10 @@
 Laser = Class{}
 
-function Laser:init(x)
-  self.x = x
-  self.y = VIRTUAL_HEIGHT / 2 + 50
+function Laser:init(x, y)
   self.width = 2
   self.height = 10
+  self.x = x - self.width / 2
+  self.y = y - self.height - 2
   self.dy = 150
 end
 
--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -2,24 +2,32 @@ PlayState = Class{includes = BaseState}
 
 function PlayState:enter(params)
   self.base = Base()
-  self.laser = Laser(self.base.x)
-  self.lasers = {"H"}
+  self.lasers = {}
 end
 
 function PlayState:update(dt)
   self.base:update(dt)
 
   if love.keyboard.wasPressed('return') then
-    table.insert(self.lasers, "bar")
+    table.insert(self.lasers, Laser(self.base.x + 0.5 * self.base.width, self.base.y))
   end
 
+  for k, laser in pairs(self.lasers) do
+    laser:update(dt)
+  end
 end
 
 function PlayState:render()
   self.base:render()
+  for k, laser in pairs(self.lasers) do
+    if laser.y < 0 then
+      table.remove(self.lasers, k)
+    end
+    laser:render()
+  end
 
   love.graphics.setFont(gFonts['menu'])
   for key,text in pairs(self.lasers) do
-    love.graphics.printf('Lasers: ' .. text, 0, VIRTUAL_HEIGHT / 2 + 40, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Lasers: ' .. tostring(table.getn(self.lasers)), 0, VIRTUAL_HEIGHT / 2 + 40, VIRTUAL_WIDTH, 'center')
   end
 end
