d7a21f1244adb964faecb92322765b0dbc386955
RyanOD <odonnell.joseph@gmail.com>
2020-11-29 10:08:08 -0800

    Minor fix. Lasers now check if enemy isActive.

--- src/classes/EnemyFormation.lua
+++ src/classes/EnemyFormation.lua
@@ -122,23 +122,28 @@ function EnemyFormation:shooterCheck()
     end
   end
 end
---]]
+
 function EnemyFormation:fireEnemyLaser()
   local col = math.random(self.cols)
-  --[[
-  for key, enemy in ipairs(self.enemy) do
-    for row=1, self.rows do
-      if self.enemy[(row-1) * self.cols + col].isActive then
-        table.insert(self.enemyLasers, Laser(self.enemy[col].xOffset + self.x + 0.5 * enemy.width, self.enemy[row].yOffset + self.y + enemy.height, -1))
-        --self.enemy[(row-1) * self.cols + col].isShooter = false
-      end
-    end
-  end--]]
-
   for row=1, self.rows do
     if not self.enemy[(row-1) * self.cols + col].isActive then
       table.insert(self.enemyLasers, Laser(self.enemy[(row-2) * self.cols + col].xOffset + self.x + self.enemy[(row-2) * self.cols + col].width * 0.5, self.enemy[(row-2) * self.cols + col].yOffset + self.y + self.enemy[(row-2) * self.cols + col].height, -1))
-      --self.enemy[(row-1) * self.cols + col].isShooter = false
+    end
+  end
+end
+--]]
+function EnemyFormation:fireEnemyLaser()
+  local shooter = self:selectShooter()
+  table.insert(self.enemyLasers, Laser(shooter.xOffset + shooter.width * 0.5 + self.x, shooter.yOffset + shooter.height + self.y, -1))
+end
+
+function EnemyFormation:selectShooter()
+  local col = math.random(self.cols)
+  for row=1, self.rows do
+    if not self.enemy[(row-1) * self.cols + col].isActive then
+      return self.enemy[(row-2) * self.cols + col]
+    elseif row == self.rows then
+      return self.enemy[(row-1) * self.cols + col]
     end
   end
 end
